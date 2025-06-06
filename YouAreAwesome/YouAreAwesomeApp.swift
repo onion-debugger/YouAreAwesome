846e88e8bfc14c9cb411b2c724e5150562b0886c
sfxmyaquob <myaquob@seamfix.com>
2025-06-06 20:51:47 +0100

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by onion_debugger on 06/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
